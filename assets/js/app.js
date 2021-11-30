51781279186cb3a6df4c6e8ceccea3b80223672d
Negindorali <vendraa.78@gmail.com>
2021-11-30 13:24:22 +0330

    add to git

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,24 @@
 
-$(".part-section").click(function (event) {
+$(".menus").click(function (event) {
     event.preventDefault();
     const id = $(this).attr('href');
     $("a.active").removeClass("active");
     $(this).addClass("active");
-
+    $('body, html').animate({
+        scrollTop: $(id).offset().top
+    }, 900);
 });
-
 
 // contact-us----------------------------------------------------
 
 $('.count').click(function () {
     const target = $(this).data('target');
-    $(this).find('i').toggleClass('fa fa-minus text-white');
-    $(this).find('i').toggleClass('fa fa-chevron-up');
+    $(this).find('').toggleClass('fa fa-minus ');
+    $(this).find('i').toggleClass('fas fa-chevron-up text-white');
     $(target).toggle(700).parent().toggleClass("targets");
 })
 // easyOrder-----------------------------------------------------
+$('.btn-sabt').click(function (){
+    swal("شماره شما با موفقیت ثبت شد." ,
+        "در سریع ترین زمان ممکن با شما تماس می گیریم!","success");
+})
